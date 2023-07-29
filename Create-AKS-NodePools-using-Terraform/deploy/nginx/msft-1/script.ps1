a0b11d276989caa87c4766a1df2fc89eadb20620
Priyanko Mukherjee <priyanko.mukherjee@gmail.com>
2023-07-29 08:49:14 +0530

    delete nginx

--- Create-AKS-NodePools-using-Terraform/deploy/nginx/msft-1/script.ps1
+++
@@ -1,7 +0,0 @@
-helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
-helm repo update
-
-helm install ingress-nginx ingress-nginx/ingress-nginx `
-    --create-namespace `
-    --namespace ingress-basic `
-    --set controller.service.annotations."service\.beta\.kubernetes\.io/azure-load-balancer-health-probe-request-path"=/healthz
