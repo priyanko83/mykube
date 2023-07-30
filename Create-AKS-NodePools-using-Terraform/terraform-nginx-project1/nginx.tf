b4d28e691955522fdcd2aca8af21f16129154635
Priyanko Mukherjee <priyanko.mukherjee@gmail.com>
2023-07-30 13:31:13 +0530

    Helm ginx routing fully automated terraform

--- Create-AKS-NodePools-using-Terraform/terraform-nginx-project1/nginx.tf
+++ Create-AKS-NodePools-using-Terraform/terraform-nginx-project1/nginx.tf
@@ -74,3 +74,12 @@ resource "helm_release" "application" {
     }
   }
 }
+
+resource "null_resource" "deploy-yaml" {
+  depends_on = [
+    helm_release.application
+  ]
+  provisioner "local-exec" {
+      command = "kubectl apply -f demo-route-app/  --namespace project1-namespace --recursive"
+  }
+}
