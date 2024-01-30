e8784b41c1291259f7ffb0a9bab30f8b4a25eee3
Priyanko Mukherjee <priyanko.mukherjee@gmail.com>
2024-01-30 22:03:33 +0530

    eshop fully working nginx separate namespace

--- eshoponcontainer/deploy/k8s/deploy-application.sh
+++ eshoponcontainer/deploy/k8s/deploy-application.sh
@@ -32,7 +32,7 @@ eshopRegistry=${ESHOP_REGISTRY}
 appPrefix="eshoplearn"
 chartsFolder="./helm-simple"
 defaultRegistry="poc12378acr.azurecr.io"
-hostIp=$(kubectl get services --namespace eshop-ns ingress-nginx-controller --output jsonpath='{.status.loadBalancer.ingress[0].ip}')
+hostIp=$(kubectl get services --namespace ingress-nginx ingress-nginx-controller --output jsonpath='{.status.loadBalancer.ingress[0].ip}')
 echo "Printing hostIp"
 echo $hostIp
 #hostIp="104.40.94.128"
